4d2596123ca335380fda141e026fa50f024c108c
choihooo <hochoi8621@naver.com>
2025-12-21 14:49:04 +0900

    feat: GitHub Packages 배포 설정 및 package.json exports 정리
    - GitHub Packages 배포 설정 추가 (.npmrc, publishConfig)
    - package.json exports 필드 정리 (불필요한 중복 제거)
    - src/index.ts에 모든 export 추가 (Button, Icon, tokens)
    - CSS export 경로 추가

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,10 @@
+// Components
 export * from "./components/button/Button";
+
+// Icons
+export * from "./icons";
+
+// Tokens
 export * from "./tokens/colors";
+export * from "./tokens/typography";
+export * from "./tokens/shadows";
